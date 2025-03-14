4c596ebf164d2f59d0a0bd0a0f56118b3fe34f0b
BugmaoLtd <begona.lopezperez@gmail.com>
2025-03-14 20:06:40 +0100

    Ejercicio 3

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,53 @@ const searchBtn = document.querySelector(".js-searchBtn");
 const searchInput = document.querySelector(".js-searchInput");
 const seriesList = document.querySelector(".js-list");
 
+let favouriteSeriesList = [];
+
+function handleFavourites(event) {
+  const idSerie = parseInt(event.currentTarget.id);
+
+  //No me trae el valor del id de series.mal_id ni series[mal_id]
+  // y por eso no funciona el find ni todo lo demás
+
+  const selectedSerie = favouriteSeriesList.find((series) => {
+    return series.mal_id === idSerie;
+  });
+
+  favouriteSeriesList.push(selectedSerie);
+  // console.log(favouriteSeriesList);
+
+  for (const favouriteSerie of favouriteSeriesList) {
+    favouriteSeriesList.innerHTML += ` <li class = "js-series" id="${series.mal_id}">
+    <img src="${serie.images.jpg.image_url}" alt="">
+    <h2>${series.title}</h2>
+  </li>`;
+  }
+}
+
+function renderSeries(series) {
+  for (const serie of series) {
+    if (
+      serie.images.jpg.image_url.src ===
+      "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+    ) {
+      seriesList.innerHTML += ` <li class = "js-series" id="${serie.mal_id}">
+    <img src="./images/logo-adalab.png" alt="">
+    <h2>${serie.title}</h2>
+  </li>`;
+    } else {
+      seriesList.innerHTML += ` <li class = "js-series" id="${serie.mal_id}">
+      <img src="${serie.images.jpg.image_url}" alt="">
+      <h2>${serie.title}</h2>
+    </li>`;
+    }
+
+    const allSeries = document.querySelectorAll(".js-series");
+    for (const singleSerie of allSeries) {
+      singleSerie.addEventListener("click", handleFavourites);
+    }
+  }
+}
+
 const handleClick = (event) => {
   event.preventDefault();
   seriesList.innerHTML = "";
@@ -19,22 +66,7 @@ const handleClick = (event) => {
     .then((response) => response.json())
     .then((data) => {
       const series = data.data;
-      for (const serie of series) {
-        if (
-          serie.images.jpg.image_url.src ===
-          "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
-        ) {
-          seriesList.innerHTML += ` <li>
-        <img src="./images/logo-adalab.png" alt="">
-        <h2>${serie.title}</h2>
-      </li>`;
-        } else {
-          seriesList.innerHTML += ` <li>
-          <img src="${serie.images.jpg.image_url}" alt="">
-          <h2>${serie.title}</h2>
-        </li>`;
-        }
-      }
+      renderSeries(series);
     });
 };
 
